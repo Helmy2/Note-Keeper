a1507d969ef288794895a3ca5c0bad9fd9d2a763
Helmy <mohamed0132525715@gmail.com>
2022-08-29 10:10:40 +0200

    Feat: add NoteActivityViewModel

--- app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -1,14 +1,14 @@
 package com.example.notekeeper;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.notekeeper.databinding.ActivityNoteBinding;
 
@@ -17,12 +17,13 @@ import java.util.List;
 public class NoteActivity extends AppCompatActivity {
     public static final String NOTE_POSITION = "com.jwhh.notekeeper.NOTE_POSITION";
     public static final int POSITION_NOT_SET = -1;
-    ActivityNoteBinding binding;
+    private ActivityNoteBinding binding;
     private NoteInfo note;
     private boolean isNewNote;
-    private List<CourseInfo> courses;
-    private boolean isCancelling;
     private int notePosition;
+    private boolean isCancelling;
+    private NoteActivityViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,64 +31,108 @@ public class NoteActivity extends AppCompatActivity {
         binding = ActivityNoteBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        courses = DataManager.getInstance().getCourses();
-        ArrayAdapter<CourseInfo> adapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, courses);
-        binding.spinnerCourses.setAdapter(adapter);
+        viewModel = new ViewModelProvider(this).get(NoteActivityViewModel.class);
+
+        if (viewModel.isNewlyCreated && savedInstanceState != null)
+            viewModel.restoreState(savedInstanceState);
+
+        viewModel.isNewlyCreated = false;
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerCourses.setAdapter(adapterCourses);
 
         readDisplayStateValues();
+        saveOriginalNoteValues();
+
         if (!isNewNote)
             displayNote();
     }
 
+    private void saveOriginalNoteValues() {
+        if (isNewNote)
+            return;
+        viewModel.originalNoteCourseId = note.getCourse().getCourseId();
+        viewModel.originalNoteTitle = note.getTitle();
+        viewModel.originalNoteText = note.getText();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isCancelling) {
+            if (isNewNote) {
+                DataManager.getInstance().removeNote(notePosition);
+            } else {
+                storePreviousNoteValues();
+            }
+        } else {
+            saveNote();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        viewModel.saveState(outState);
+    }
+
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(viewModel.originalNoteCourseId);
+        note.setCourse(course);
+        note.setTitle(viewModel.originalNoteTitle);
+        note.setText(viewModel.originalNoteText);
+    }
+
+    private void saveNote() {
+        note.setCourse((CourseInfo) binding.spinnerCourses.getSelectedItem());
+        note.setTitle(binding.textNoteTitle.getText().toString());
+        note.setText(binding.textNoteText.getText().toString());
+    }
+
     private void displayNote() {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
         int courseIndex = courses.indexOf(note.getCourse());
         binding.spinnerCourses.setSelection(courseIndex);
-        binding.textNoteText.setText(note.getText());
         binding.textNoteTitle.setText(note.getTitle());
+        binding.textNoteText.setText(note.getText());
     }
 
     private void readDisplayStateValues() {
         Intent intent = getIntent();
         int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
         isNewNote = position == POSITION_NOT_SET;
-        if (isNewNote)
-            createNode();
-        else
+        if (isNewNote) {
+            createNewNote();
+        } else {
             note = DataManager.getInstance().getNotes().get(position);
+        }
     }
 
-    private void createNode() {
-        DataManager dataManager = DataManager.getInstance();
-        notePosition = dataManager.createNewNote();
-        note = dataManager.getNotes().get(notePosition);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (!isCancelling)
-            if (isNewNote)
-                DataManager.getInstance().removeNote(notePosition);
-            else
-                saveNote();
-    }
-
-    private void saveNote() {
-        note.setCourse((CourseInfo) binding.spinnerCourses.getSelectedItem());
-        note.setText(binding.textNoteText.getText().toString());
-        note.setTitle(binding.textNoteTitle.getText().toString());
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        notePosition = dm.createNewNote();
+        note = dm.getNotes().get(notePosition);
     }
 
     @Override
     public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_note, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_send_mail) {
             sendEmail();
             return true;
---
+++ app/src/main/java/com/example/notekeeper/NoteActivityViewModel.java
@@ -0,0 +1,33 @@
+package com.example.notekeeper;
+
+import android.os.Bundle;
+
+import androidx.lifecycle.ViewModel;
+
+public class NoteActivityViewModel extends ViewModel {
+    public static final String ORIGINAL_NOTE_COURSE_ID = "com.jwhh.notekeeper.ORIGINAL_NOTE_COURSE_ID";
+    public static final String ORIGINAL_NOTE_TITLE = "com.jwhh.notekeeper.ORIGINAL_NOTE_TITLE";
+    public static final String ORIGINAL_NOTE_TEXT = "com.jwhh.notekeeper.ORIGINAL_NOTE_TEXT";
+
+    public String originalNoteCourseId;
+    public String originalNoteTitle;
+    public String originalNoteText;
+    public boolean isNewlyCreated = true;
+
+    public void saveState(Bundle outState) {
+        outState.putString(ORIGINAL_NOTE_COURSE_ID, originalNoteCourseId);
+        outState.putString(ORIGINAL_NOTE_TITLE, originalNoteTitle);
+        outState.putString(ORIGINAL_NOTE_TEXT, originalNoteText);
+    }
+
+    public void restoreState(Bundle inState) {
+        originalNoteCourseId = inState.getString(ORIGINAL_NOTE_COURSE_ID);
+        originalNoteTitle = inState.getString(ORIGINAL_NOTE_TEXT);
+        originalNoteText = inState.getString(ORIGINAL_NOTE_TITLE);
+    }
+}
+
+
+
+
+
